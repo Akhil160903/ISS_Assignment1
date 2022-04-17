6bce0f5106c9915a7609a63f8521c781f2c68cbf
Akhil Gupta <102593031+LemonLizzie@users.noreply.github.com>
2022-04-17 12:06:30 +0530

    Delete q3e.sh

--- q3e.sh
+++
@@ -1,8 +0,0 @@
-sed -e 's/[^[:alpha:]]/ /g' quotes.txt | tr '\n' " " |  tr -s " " | tr " " '\n'| tr 'A-Z' 'a-z' | 
-sort | uniq -c | sort -nr |while read count name
-do
-        if [ ${count} -gt 1 ]
-        then
-                echo "${name} ${count}"
-        fi
-done
